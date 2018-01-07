6c30de190c3bf480e01eb77b127ea20c3460ddf7
Andrew Esterhuizen <andrewe22@gmail.com>
2018-01-07 17:17:07 +0000

    added Machine component

---
+++ src/components/Machine.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Machine.css';
+
+class Machine extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return(
+            <div className="Machine">
+                
+            </div>
+        )
+    }
+}
+
+export default Machine;
