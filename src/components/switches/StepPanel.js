e2b45f19593fa00477a6ba5688dd4e0b4ffd447f
Andrew Esterhuizen <andrewe22@gmail.com>
2018-01-09 11:55:41 +0000

    added dynamic loading for drum machine steps

--- src/components/switches/StepPanel.js
+++ src/components/switches/StepPanel.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './StepPanel.css';
 import Step from './Step.js';
 
+
 class StepPanel extends Component {
     render() {
         let switches = [];
         for(let i = 0; i < 16; i++) {
-            switches.push(<Step label={i+1}key={`switch-${i}`}/>);
+            switches.push();
         }
         return (
             <div className="StepPanel">
                 {switches}
+                { this.props.drums.map( (drum, i) => {
+                    return <Step label={drum.name}key={`switch-${i}`}/>
+                })}
             </div>
         )
     }
