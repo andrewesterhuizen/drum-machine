2d5439463d345aa8c56f2c2bd34794b6eab44876
Andrew Esterhuizen <ae@andrews-mbp.home>
2019-02-02 10:35:15 +0000

    new loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-class Loading extends Component {
-	render() {
-		return (
-			<div>
-				<h1> loading </h1>
-			</div>
-		);
-	}
-}
+const map = (num, in_min, in_max, out_min, out_max) =>
+  ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 
+const getPos = (height, heightOffset, time, offset) => {
+  const sinValue = Math.sin(time + offset);
+  const mapped = map(sinValue, -1, 1, 0, height - heightOffset);
+  const edgeOffset = mapped + heightOffset / 2;
+
+  return edgeOffset;
+};
+
+const Loading = () => {
+  const [time, setTime] = useState(0);
+
+  let height = 60;
+  let heightOffset = height / 5;
+  let animate = true;
+
+  const update = () => {
+    if (!animate) return;
+    setTime(time + 0.07);
+    requestAnimationFrame(update);
+  };
+
+  useEffect(() => {
+    requestAnimationFrame(update);
+    return () => {
+      animate = false;
+    };
+  });
+
+  return (
+    <svg width={100} height={height}>
+      <circle
+        cx="20%"
+        cy={getPos(height, heightOffset, time, 0)}
+        r={4}
+        fill="#fff"
+      />
+      <circle
+        cx="35%"
+        cy={getPos(height, heightOffset, time, 1)}
+        r={4}
+        fill="#fff"
+      />
+      <circle
+        cx="50%"
+        cy={getPos(height, heightOffset, time, 2)}
+        r={4}
+        fill="#fff"
+      />
+      <circle
+        cx="65%"
+        cy={getPos(height, heightOffset, time, 3)}
+        r={4}
+        fill="#fff"
+      />
+      <circle
+        cx="80%"
+        cy={getPos(height, heightOffset, time, 4)}
+        r={4}
+        fill="#fff"
+      />
+    </svg>
+  );
+};
 
 export default Loading;
