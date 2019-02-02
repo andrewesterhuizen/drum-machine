d790ae415b74976a5b7e2fcf432ec663c5a7b94b
Andrew Esterhuizen <ae@andrews-mbp.home>
2019-02-02 10:36:30 +0000

    Updates

---
+++ precache-manifest.1bac68e6e8f72ab5f80d8910ed1168e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "001b8f31198ab9afa1e6",
+    "url": "/static/js/main.001b8f31.chunk.js"
+  },
+  {
+    "revision": "87baebcfa77500722738",
+    "url": "/static/js/1.87baebcf.chunk.js"
+  },
+  {
+    "revision": "001b8f31198ab9afa1e6",
+    "url": "/static/css/main.cd4c1df3.chunk.css"
+  },
+  {
+    "revision": "7b48b5adab9f5d0d596247ff9f2db648",
+    "url": "/index.html"
+  }
+];
